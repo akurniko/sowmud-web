dcc22c868b693ddc848ff9451090323decd2cb29
Arseny Kurnikov <akurniko@gmail.com>
2022-05-29 08:51:16 +0300

    Start lein adjustments

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject sowmud-web "0.0.1"
   :description "Clojure Discord-IRC gateway"
   :dependencies [[org.clojure/clojure   "1.8.0"]
-                 [org.pircbotx/pircbotx "1.9"]])
+                 [org.pircbotx/pircbotx "1.9"]]
+  :repl-options {:init-ns sowmud-web.core}
+  :main sowmud-web.core)
