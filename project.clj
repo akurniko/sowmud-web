ca9d599dc09934cf9150c2e7a66cdf9f92d5b0ca
Arseny Kurnikov <akurniko@gmail.com>
2022-06-14 07:10:35 +0300

    Add async dep into lein config

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Clojure Discord-IRC gateway"
   :dependencies [[clj-irc "0.0.3-SNAPSHOT"]
                  [org.clojure/clojure   "1.8.0"]
+                 [org.clojure/core.async   "1.5.648"]
                  [org.pircbotx/pircbotx "1.9"]]
+
   :repl-options {:init-ns sowmud-web.core}
   :main sowmud-web.core)
