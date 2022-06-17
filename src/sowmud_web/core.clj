71b6082f72ac43ec4df1403de5b4d99645ba931f
Arseny Kurnikov <akurniko@gmail.com>
2022-06-17 07:00:00 +0300

    Add DS state into core

--- src/sowmud_web/core.clj
+++ src/sowmud_web/core.clj
@@ -7,6 +7,39 @@
             [discljord.formatting :refer [mention-user]]
             [discljord.events :refer [message-pump!]]))
 
+(def state (atom nil))
+(def bot-id (atom nil))
+(def config (edn/read-string (slurp "config.edn")))
+
+(defn random-response [user]
+  (str (rand-nth (:responses config)) ", " (mention-user user) \!))
+
+(defmulti handle-event (fn [type _data] type))
+
+(defmethod handle-event :message-create
+  [_ {:keys [channel-id author mentions] :as _data}]
+  (when (some #{@bot-id} (map :id mentions))
+    (discord-rest/create-message! (:rest @state) channel-id :content (random-response author))))
+
+(defmethod handle-event :ready
+  [_ _]
+  (discord-ws/status-update! (:gateway @state) :activity (discord-ws/create-activity :name (:playing config))))
+
+(defmethod handle-event :default [_ _])
+
+(defn start-bot! [token & intents]
+  (let [event-channel (chan 100)
+        gateway-connection (discord-ws/connect-bot! token event-channel :intents (set intents))
+        rest-connection (discord-rest/start-connection! token)]
+    {:events  event-channel
+     :gateway gateway-connection
+     :rest    rest-connection}))
+
+(defn stop-bot! [{:keys [rest gateway events] :as _state}]
+  (discord-rest/stop-connection! rest)
+  (discord-ws/disconnect-bot! gateway)
+  (close! events))
+
 (clj-irc.core/defbot {:nick "sowmud-web" :host "irc.libera.chat"
          :channels ["#sowmud"]
          :auto-reconnect false}
