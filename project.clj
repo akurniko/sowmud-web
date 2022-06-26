3ea5a6b81839e4cabd2a2a3be5b967540591b448
Arseny Kurnikov <akurniko@gmail.com>
2022-06-26 06:57:33 +0300

    Update Clojure to 1.9

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject sowmud-web "0.0.1"
   :description "Clojure Discord-IRC gateway"
   :dependencies [[clj-irc "0.0.3-SNAPSHOT"]
-                 [org.clojure/clojure   "1.8.0"]
+                 [org.clojure/clojure   "1.9.0"]
                  [org.clojure/core.async   "1.5.648"]
                  [org.pircbotx/pircbotx "1.9"]
                  [com.github.discljord/discljord "1.3.1"]
