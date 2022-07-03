7393ff0d7452f29299ddb843ca3283e9687b361b
Arseny Kurnikov <akurniko@gmail.com>
2022-07-03 08:53:59 +0300

    Start moving Discljord into core

--- src/sowmud_web/core.clj
+++ src/sowmud_web/core.clj
@@ -1,7 +1,7 @@
 (ns sowmud-web.core
   (:require [clj-irc.core]
             [clojure.edn :as edn]
-            [clojure.core.async :refer [chan close!]]
+            [clojure.core.async :refer [chan close!] :as a]
             [discljord.messaging :as discord-rest]
             [discljord.connections :as discord-ws]
             [discljord.formatting :refer [mention-user]]
@@ -53,3 +53,21 @@
     (message-pump! (:events @state) handle-event)
     (finally (stop-bot! @state))))
 
+
+(def token "TOKEN")
+(def intents #{:guilds :guild-messages})
+
+(let [event-ch      (a/chan 100)
+      connection-ch (discord-rest/connect-bot! token event-ch :intents intents)
+      message-ch    (discord-ws/start-connection! token)]
+  (try
+    (loop []
+      (let [[event-type event-data] (a/<!! event-ch)]
+        (println "í¾‰ NEW EVENT! í¾‰")
+        (println "Event type:" event-type)
+        (println "Event data:" (pr-str event-data))
+        (recur)))
+    (finally
+      (discord-ws/stop-connection! message-ch)
+      (discord-rest/disconnect-bot!  connection-ch)
+      (a/close!           event-ch))))
