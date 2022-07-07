2254efdbe323a1f10302ad1dcca6182fa8939c7f
Arseny Kurnikov <akurniko@gmail.com>
2022-07-07 06:38:55 +0300

    Attempt to remove irrelevant bot parts

--- src/sowmud_web/core.clj
+++ src/sowmud_web/core.clj
@@ -14,44 +14,11 @@
 (defn random-response [user]
   (str (rand-nth (:responses config)) ", " (mention-user user) \!))
 
-(defmulti handle-event (fn [type _data] type))
-
-(defmethod handle-event :message-create
-  [_ {:keys [channel-id author mentions] :as _data}]
-  (when (some #{@bot-id} (map :id mentions))
-    (discord-rest/create-message! (:rest @state) channel-id :content (random-response author))))
-
-(defmethod handle-event :ready
-  [_ _]
-  (discord-ws/status-update! (:gateway @state) :activity (discord-ws/create-activity :name (:playing config))))
-
-(defmethod handle-event :default [_ _])
-
-(defn start-bot! [token & intents]
-  (let [event-channel (chan 100)
-        gateway-connection (discord-ws/connect-bot! token event-channel :intents (set intents))
-        rest-connection (discord-rest/start-connection! token)]
-    {:events  event-channel
-     :gateway gateway-connection
-     :rest    rest-connection}))
-
-(defn stop-bot! [{:keys [rest gateway events] :as _state}]
-  (discord-rest/stop-connection! rest)
-  (discord-ws/disconnect-bot! gateway)
-  (close! events))
-
 (clj-irc.core/defbot {:nick "sowmud-web" :host "irc.libera.chat"
          :channels ["#sowmud"]
          :auto-reconnect false}
         (clj-irc.core/on-message {:keys [content]} reply-to {:regexp #"^test:.*"}
           (reply-to (str "did you really say :" content))))
-
-(defn -main [& args]
-  (reset! state (start-bot! (:token config) :guild-messages))
-  (reset! bot-id (:id @(discord-rest/get-current-user! (:rest @state))))
-  (try
-    (message-pump! (:events @state) handle-event)
-    (finally (stop-bot! @state))))
 
 
 (def token "TOKEN")
