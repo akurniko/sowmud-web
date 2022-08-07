9eebd0612ca5238792a4934d6048c4d2bac374c9
Arseny Kurnikov <akurniko@gmail.com>
2022-08-07 09:53:30 +0300

    Add channel for IRC bot

--- src/sowmud_web/core.clj
+++ src/sowmud_web/core.clj
@@ -5,8 +5,17 @@
             [discljord.formatting :refer [mention-user]]
             [discljord.events :refer [message-pump!]]))
 
-(clj-irc.core/defbot {:nick "sowmud-web" :host "irc.libera.chat"
-         :channels ["#sowmud"]
-         :auto-reconnect false}
-        (clj-irc.core/on-message {:keys [content]} reply-to {:regexp #"^test:.*"}
-          (reply-to (str "did you really say :" content))))
+(defn msg_relay [c msg]
+  (a/go (a/>! c msg))
+  (reply-to (str "did you really say:" (a/<!! (a/go (a/<! c))))))
+
+(let [c (chan)]
+  (clj-irc.core/defbot {:nick "sowmud-web" :host "irc.libera.chat"
+           :channels ["#sowmud"]
+           :auto-reconnect false}
+          (clj-irc.core/on-message {:keys [content]} reply-to {:regexp #"^test:.*"}
+            (msg_relay c content))))
+
+;;  (go (>! c "hello"))
+;;  (assert (= "hello" (<!! (go (<! c)))))
+;;  (close! c))
