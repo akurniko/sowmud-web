ecb56310bd9a85be7ec803a2d044a806b1637be8
Arseny Kurnikov <akurniko@gmail.com>
2022-08-11 10:18:39 +0300

    Make IRC bot work with async

--- src/sowmud_web/core.clj
+++ src/sowmud_web/core.clj
@@ -4,16 +4,17 @@
             [clojure.edn :as edn]
             [discljord.events :refer [message-pump!]]))
 
-(defn msg_relay [c msg]
+(defn msg_relay [c msg reply-to]
   (a/go (a/>! c msg))
-  (str "did you really say:" (a/<!! (a/go (a/<! c)))))
+  (println msg)
+  (reply-to (str "did you really say:" (a/<!! (a/go (a/<! c))))))
 
 (let [c (a/chan)]
   (clj-irc.core/defbot {:nick "sowmud-web" :host "irc.libera.chat"
            :channels ["#sowmud"]
            :auto-reconnect false}
           (clj-irc.core/on-message {:keys [content]} reply-to {:regexp #"^test:.*"}
-            (msg_relay c content))))
+            (msg_relay c content reply-to))))
 ;; (add-handler (fn[m r]
 ;;                  (r (str "You just said" (:content m)))))
 
