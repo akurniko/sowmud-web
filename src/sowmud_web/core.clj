7eb9f80730c8f92626006f3832bc9bbb62527d80
Arseny Kurnikov <akurniko@gmail.com>
2022-08-12 12:27:04 +0300

    Make Discord bot work with async

--- src/sowmud_web/core.clj
+++ src/sowmud_web/core.clj
@@ -2,6 +2,8 @@
   (:require [clj-irc.core]
             [clojure.core.async :as a]
             [clojure.edn :as edn]
+            [discljord.messaging :as discord-rest]
+            [discljord.connections :as discord-ws]
             [discljord.events :refer [message-pump!]]))
 
 (defn msg_relay [c msg reply-to]
@@ -15,6 +17,25 @@
            :auto-reconnect false}
           (clj-irc.core/on-message {:keys [content]} reply-to {:regexp #"^test:.*"}
             (msg_relay c content reply-to))))
+
+(def token "")
+(def intents #{:guilds :guild-messages})
+
+(let [event-ch      (a/chan 100)
+      connection-ch (discord-ws/connect-bot! token event-ch :intents intents)
+      message-ch    (discord-rest/start-connection! token)]
+  (a/go 
+    (try
+    (loop []
+      (let [[event-type event-data] (a/<!! event-ch)]
+        (println "í¾‰ NEW EVENT! í¾‰")
+        (println "Event type:" event-type)
+        (println "Event data:" (pr-str event-data))
+        (recur)))
+    (finally
+      (discord-rest/stop-connection! message-ch)
+      (discord-ws/disconnect-bot! connection-ch)
+      (a/close!           event-ch)))))
 ;; (add-handler (fn[m r]
 ;;                  (r (str "You just said" (:content m)))))
 
